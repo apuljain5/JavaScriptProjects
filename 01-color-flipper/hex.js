233218083b0097a8b1ac74e3fb6ac2acd1b264e5
Apul Jain <apuljain5@gmail.com>
2022-06-02 18:07:57 +0530

    modified hex.js for complex functionality

--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,1 +1,19 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  //console.log(document.body);
+  const randomHex = getRandomHex();
+  document.body.style.backgroundColor = randomHex;
+  color.textContent = randomHex;
+});
+
+function getRandomHex() {
+  let randomHex = "#";
+  for (let i = 0; i < 6; i++) {
+    randomHex += hex[Math.floor(Math.random() * hex.length)];
+  }
+  return randomHex;
+}
